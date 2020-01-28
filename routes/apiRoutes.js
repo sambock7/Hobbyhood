7f55d0a159e160424c67ba65eb8ea0f0296dbeb6
k-lord <katielord27@gmail.com>
2020-01-27 19:09:52 -0500

    commented out error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,10 +11,11 @@ module.exports = function (app) {
     })
   );
 
-  app.get("/login", function(req, res) {
+  /*app.get("/login", function(req, res) {
     var user_id = req.user.user_id
     console.log(user_id);
   })
+  */
 
 
 
